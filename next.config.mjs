1598fadcb86db1e7b75c9caf7a866b9eb955fd75
Isaac Wasserman <isaacrwasserman@gmail.com>
2024-02-15 21:38:18 -0500

    ditto

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
     images: {
         // domains: ["cdn.sanity.io"],
-        remotePatterns: ["https://cdn.sanity.io/**"],
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: 'cdn.sanity.io',
+            port: '',
+            pathname: '**',
+          }],
     },
     typescript: {
         // !! WARN !!
